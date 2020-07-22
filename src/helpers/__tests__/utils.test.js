2220bc65ca7e0b863becb060371137216d22e6a1
InBrewJ <jason@urawizard.com>
2020-07-22 16:46:49 +0100

    half lame first attempt at 11 Container With Most Water

--- src/helpers/__tests__/utils.test.js
+++ src/helpers/__tests__/utils.test.js
@@ -1,4 +1,4 @@
-const { adder, HelloLBHer } = require('../utils')
+const { adder, HelloLBHer, maxArea } = require('../utils')
 
 describe('Adder', () => {
   test('it should add things properly', () => {
@@ -9,5 +9,37 @@ describe('Adder', () => {
 describe('HelloLBHer', () => {
   test('it should say hi', () => {
     expect(HelloLBHer()).toEqual('Hey, LBH')
+  })
+})
+
+describe.only('"Container With Most Water" from LeetCode', () => {
+  test.only('[1, 8, 6, 2, 5, 4, 8, 3, 7] should return 49', () => {
+    const heights = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+    expect(maxArea(heights)).toEqual(49)
+  })
+
+  test('Seven ones should return 6', () => {
+    const heights = [1, 1, 1, 1, 1, 1, 1]
+    expect(maxArea(heights)).toEqual(6)
+  })
+
+  test('[1,1,3,4,2,1,4] should return 16', () => {
+    const heights = [1, 1, 3, 4, 2, 1, 4]
+    expect(maxArea(heights)).toEqual(16)
+  })
+
+  test('[4,2,1,1,3,3,3] should return 18', () => {
+    const heights = [4, 2, 1, 1, 3, 3, 3]
+    expect(maxArea(heights)).toEqual(18)
+  })
+
+  test('[1,1,16,16,1] should return 16', () => {
+    const heights = [1, 1, 16, 16, 1]
+    expect(maxArea(heights)).toEqual(16)
+  })
+
+  test.only('[1,2,1] should return 2', () => {
+    const heights = [1, 2, 1]
+    expect(maxArea(heights)).toEqual(2)
   })
 })
--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -6,7 +6,74 @@ const HelloLBHer = () => {
   return 'Hey, LBH'
 }
 
+Math.clip = function (number, min, max) {
+  return Math.max(min, Math.min(number, max))
+}
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+  // In general, need to:
+  // Get highest heights
+  // But !!also!! maximise the distance between heights
+  // to, at the end, maximise the area
+
+  // Find first max in the array
+  // From ltr, find the next max
+
+  let firstMaxIndex = 0
+  let lastMaxIndex = 0
+  let firstMaxValue = 0
+  let lastMaxValue = 0
+  let distanceBetween = 0
+  let maxArea = 0
+  const endIndex = height.length - 1
+
+  height.forEach((h, i) => {
+    // in-loop util for getting the current area
+    const getArea = (lastIndex, firstIndex) => {
+      distanceBetween = Math.clip(lastIndex - firstIndex, 0, height.length)
+      return Math.min(height[lastIndex], height[firstIndex]) * distanceBetween
+    }
+
+    const consideredAreaIsBigger = (lmi, fmi) => {
+      return getArea(lmi, fmi) > maxArea
+    }
+
+    // Really awkward bounds checking...
+    if (i === 0) {
+      firstMaxValue = h
+      firstMaxIndex = 0
+      lastMaxValue = height[1]
+      lastMaxIndex = 1
+    } else if (i === endIndex) {
+      if (consideredAreaIsBigger(lastMaxIndex, firstMaxIndex)) {
+        lastMaxValue = height[endIndex]
+        lastMaxIndex = endIndex
+      }
+    }
+
+    if (h > firstMaxValue) {
+      firstMaxValue = h
+      firstMaxIndex = i
+    }
+
+    if (consideredAreaIsBigger(lastMaxIndex, firstMaxIndex)) {
+      maxArea = getArea(lastMaxIndex, firstMaxIndex)
+      // console.log(`maxArea set to ${maxArea}`)
+      // console.log(`firstMaxIndex ${firstMaxIndex}`)
+      // console.log(`lastMaxIndex ${lastMaxIndex}`)
+      // console.log(`height ${height}`)
+    }
+  })
+
+  return maxArea
+}
+
 module.exports = {
   adder,
-  HelloLBHer
+  HelloLBHer,
+  maxArea
 }
